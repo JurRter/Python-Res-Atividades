9469320dda54ecf1f487e8e62bf38af1b0959658
JurRter <brendo.henrique@mail.uft.edu.br>
2026-05-26 17:39:00 -0300

    Resolução da terceira atividade

--- Ativiades-2semana/Atividade3.py
+++ Ativiades-2semana/Atividade3.py
@@ -6,3 +6,16 @@
 #]
 #Faça um print para exibir especificamente a nota 8.5 acessando os índices corretos (linha e coluna).
 #Use um loop for aninhado (um for dentro de outro for) para percorrer a matriz e imprimir todas as notas, uma por uma.
+
+notas_alunos = [
+   [7.5, 8.0, 9.0], 
+   [6.0, 7.5, 8.5]
+]
+
+print(notas_alunos[1][2])
+
+for aluno in range(2):
+    print("Aluno %i: " %(aluno+1))
+    for prova in range(3):
+        print("Prova %i: " %(prova+1))
+        print(notas_alunos[aluno][prova])

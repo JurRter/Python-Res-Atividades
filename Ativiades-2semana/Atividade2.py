ba699e48097f983303a07542bf9f9ca77aac2ecd
JurRter <brendo.henrique@mail.uft.edu.br>
2026-05-26 17:19:55 -0300

    Resolução da segunda atividade

--- Ativiades-2semana/Atividade2.py
+++ Ativiades-2semana/Atividade2.py
@@ -3,3 +3,10 @@
 
 #Se o usuário digitar um número positivo ou zero, o programa deve apenas continuar pedindo o próximo número.
 #Se o usuário digitar um número negativo (menor que 0), o programa deve usar o comando break para encerrar o loop e imprimir a mensagem "Programa encerrado!".
+
+while(True):
+    numero = int(input("Digite seu numero: "))
+    if(numero < 0):
+        break
+
+print("Programa encerrado!")
